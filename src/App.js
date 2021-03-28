fa5dff8a5d1a3e8a6e069b47dea094555a01bfb5
Imran <martanec@bk.ru>
2021-03-28 11:05:04 +0300

    выгрузка проекта на Heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {loadTodo} from "./actions";
+import {useEffect} from "react";
+import "./style.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const todos = useSelector(state => state.todos)
+    const loading = useSelector(state => state.loading)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadTodo());
+    }, []);
+
+    return (
+        <div>
+            <div className="container">
+                <div>
+                    <h1>График лицензирования организаций</h1>
+                    <div className="main-line">
+                        <h3>не прошли лицензирование -</h3>
+                        <div className="line-red"></div>
+                    </div>
+                    <div className="main-line">
+                        <h3>прошли лицензирование -</h3>
+                        <div className="line-blue"></div>
+                    </div>
+                </div>
+                <div className="row">
+                    {loading ? <div className="load">идет загрузка</div> : (
+                        todos.map(item => {
+                                return(
+                                    <div className={item.completed ? "card blue" : " card red"}>
+                                        <h3>
+                                            {item.id}
+                                        </h3>
+                                        <div className="title">
+                                            {item.title}
+                                        </div>
+                                    </div>
+                                )
+                            }
+
+                        ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/actions.js
@@ -0,0 +1,14 @@
+export const loadTodo = () => {
+    return (dispatch) => {
+        dispatch({type: "start"})
+
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then(response => response.json())
+            .then(json => {
+                dispatch({
+                    type: "load",
+                    payload: json
+                })
+            })
+    }
+}
